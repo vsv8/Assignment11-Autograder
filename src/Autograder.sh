228d494c239d07559f5fc17bc9bcbe18f81b4368
Arnarleo <32820528+Arnarleo@users.noreply.github.com>
2021-04-15 13:27:37 +0000

    Create Autograder.sh

---
+++ src/Autograder.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Welcome to Autograder"
+java -cp "C:\Program Files\JetBrains\IntelliJ IDEA 2019.2.1\lib\junit-4.12.jar;C:\Users\Arnarleo\Desktop\Master\Profun\MoneyStack\libs\mockito-core-3.8.0.jar;C:\Users\Arnarleo\Desktop\Master\Profun\MoneyStack\libs\byte-buddy-1.10.20.jar;C:\Users\Arnarleo\Desktop\Master\Profun\MoneyStack\libs\byte-buddy-agent-1.10.20.jar;C:\Users\Arnarleo\Desktop\Master\Profun\MoneyStack\libs\objenesis-3.1.jar;C:\Users\Arnarleo\Desktop\Master\Profun\MoneyStack\libs\hamcrest-all-1.3.jar;C:\Users\Arnarleo\Desktop\Master\Profun\MoneyStack\bin" org.junit.runner.JUnitCore AllTests
